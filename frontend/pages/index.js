21c4996d3b56056f2d3afe2aa0aeb0f635db26fb
Alvin Rumbaoa <alvinrumbaoa@gmail.com>
2022-08-20 09:47:18 -0700

    70% done

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,18 @@
-import {Box, Heading, Image, Text, Button, useColorModeValue, Flex, GridItem,  Grid} from '@chakra-ui/react'
+import {Box, Heading, Image, Text,HStack, VStack, Button,Container, useColorModeValue, Flex, GridItem,  Grid} from '@chakra-ui/react'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import { motion } from "framer-motion";
+import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons'
 
 const MotionFlex = motion(Flex)
 export default function Home() {
 
-
+  const images = [
+    "/images/image-slide-1.jpg",
+    "/images/image-slide-2.jpg",
+    "/images/image-slide-3.jpg",
+    "/images/image-slide-4.jpg",
+   "/images/image-slide-5.jpg",
+    ]; 
 
   return (
 
@@ -21,13 +27,14 @@ export default function Home() {
           <Text w="600px" fontSize={{sm:"17px",md:"18px",lg:"19px"}} color="#7A746E" textAlign="center" m={4}> With over two years of experience in various design disciplines, I’m your one-stop shop for your design needs.</Text>
 
           <Grid  h='200px' w={{md:"100%",lg:"50%"}} templateRows='repeat(2, 1fr)' templateColumns={{sm:'repeat(2, 1fr)', lg:'repeat(6, 1fr)'}} m={4} gap={3}>
-            <GridItem p={2}  borderRadius={5} rowSpan={{sm:1,md:1,lg:2}} colSpan={2} bg='#755CDE' direction='row' justify="space-between"><Image p={2} ml={120} src="/images/Graphic Design.png" width="180" height="180" justify="flex-end"/><Text mt={120} p={2} color="white" fontWeight="900" justify="flex-end" alignItems="flex-end">Graphic Design</Text></GridItem>
-            <GridItem p={2} borderRadius={5} colSpan={1} bg='#F6A560' ><Image p={2}  ml={61}  src="/images/ui.png" width="55" height="55" justify="flex-end"/><Text mt={20} color="white" fontWeight="900" fontSize="lg">UX/UI</Text> </GridItem>
-            <GridItem p={2} borderRadius={5} colSpan={1} bg='#F39E9E' ><Image p={2} ml={61} src="/images/apps.svg" width="55" height="55"  justify="flex-end"/><Text mt={20} color="white" fontWeight="900">Apps</Text> </GridItem>
-            <GridItem p={2} borderRadius={5} colSpan={2} bg='#61C4B7' ><Image p={2}  ml={140} src="/images/photo.png" width="55" height="55"justify="flex-end"/><Text mt={20} color="white" fontWeight="900">Photography</Text> </GridItem>
-            <GridItem p={2} borderRadius={5} colSpan={2} bg='#EB7565' ><Image p={2}  ml={160} src="/images/ illustration.png" width="55" height="55" justify="flex-end"/><Text mt={20} color="white" fontWeight="900">Illustrations</Text> </GridItem>
-            <GridItem p={2} borderRadius={5} colSpan={2} bg='#552049' ><Image p={2}  ml={160} src="/images/motion.png" width="55" height="55" justify="flex-end"/><Text mt={20} color="white" fontWeight="900">Motion Graphics</Text> </GridItem>
+            <GridItem p={2}  borderRadius={5} rowSpan={{sm:1,md:1,lg:2}} colSpan={2} bg='#755CDE' direction='row' justify="space-between" _hover={{bg:"#000000"}}><Image p={2} ml={120} src="/images/Graphic Design.png" width="180" height="180" justify="flex-end"/><Text mt={120} p={2} color="white" fontWeight="900" justify="flex-end" alignItems="flex-end">Graphic Design</Text></GridItem>
+            <GridItem p={2} borderRadius={5} colSpan={1} bg='#F6A560' _hover={{bg:"#000000"}}><Image p={2}  ml={61}  src="/images/ui.png" width="55" height="55" justify="flex-end"/><Text mt={20} color="white" fontWeight="900" fontSize="lg">UX/UI</Text> </GridItem>
+            <GridItem p={2} borderRadius={5} colSpan={1} bg='#F39E9E' _hover={{bg:"#000000"}}><Image p={2} ml={61} src="/images/apps.svg" width="55" height="55"  justify="flex-end"/><Text mt={20} color="white" fontWeight="900">Apps</Text> </GridItem>
+            <GridItem p={2} borderRadius={5} colSpan={2} bg='#61C4B7' _hover={{bg:"#000000"}}><Image p={2}  ml={140} src="/images/photo.png" width="55" height="55"justify="flex-end"/><Text mt={20} color="white" fontWeight="900">Photography</Text> </GridItem>
+            <GridItem p={2} borderRadius={5} colSpan={2} bg='#EB7565' _hover={{bg:"#000000"}}><Image p={2}  ml={160} src="/images/ illustration.png" width="55" height="55" justify="flex-end"/><Text mt={20} color="white" fontWeight="900">Illustrations</Text> </GridItem>
+            <GridItem p={2} borderRadius={5} colSpan={2} bg='#552049' _hover={{bg:"#000000"}}><Image p={2}  ml={160} src="/images/motion.png" width="55" height="55" justify="flex-end"/><Text mt={20} color="white" fontWeight="900">Motion Graphics</Text> </GridItem>
           </Grid>
+
           <Flex m={20} display="flex" justify="space-around" direction="row" mt={300}>
             <MotionFlex whileHover={{ scale: 1.1}}  whileTap={{ scale: 0.9 }} >
                <Image src="/images/alvin.png" h="440" w="440" />
@@ -38,6 +45,39 @@ export default function Home() {
               <Button mt={5} p={6} w="228px" h="56px" borderRadius="30px" color="white" bg="#EB7565" _hover={{ bg: '#F6A560' }} >Free Consultation </Button>
             </Flex>
           </Flex>
+
+          <Heading p={22}>My Work</Heading>
+      
+								<HStack
+								w="full"
+								pb={22}
+								overflowX="hidden"
+								spacing={8}
+								scrollSnapType="x mandatory"
+								position="relative"
+								zIndex="docked"
+                p={22}
+								>
+								{images.map((src) => (
+									<Image
+									key={src}
+									src={src}
+									rounded="md"
+									shadow="lg"
+									h={80}
+									scrollSnapAlign="center"
+									maxWidth="initial"
+									/>
+								))}
+								</HStack >
+                <HStack  p={22} justify="center">
+                <ArrowBackIcon p={2} w={10} h={10} bg="#000000" _hover={{bg:"#755CDE"}} color="white" borderRadius="170px" /> 
+                <ArrowForwardIcon p={2} w={10} h={10} bg="#000000" _hover={{bg:"#755CDE"}} color="white" borderRadius="170px"/>
+                </HStack>
+               
+
+      	
+
         </Flex>
   )
 }
