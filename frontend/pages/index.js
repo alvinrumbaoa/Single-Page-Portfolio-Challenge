2be01d4dd64ba9f64fcccc6873f5ad65a26603bf
Alvin Rumbaoa <alvinrumbaoa@gmail.com>
2022-08-20 22:37:27 -0700

    save complate challenge

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
           <Text w={{sm:"300px",lg:"600px"}} fontSize={{sm:"17px",md:"18px",lg:"19px"}} color="#7A746E" textAlign="center" m={4}> With over two years of experience in various design disciplines, I’m your one-stop shop for your design needs.</Text>
 
           <Grid  h={{sm:"100%",lg:'200px'}} w={{md:"100%",lg:"50%"}} templateRows='repeat(2, 1fr)' templateColumns={{sm:'repeat(2, 1fr)', lg:'repeat(6, 1fr)'}} m={4} gap={3}>
-            <GridItem h={{sm:"200px"}} p={2}  borderRadius={5} rowSpan={{sm:1,md:1,lg:2}} colSpan={2} bg='#755CDE' direction='row' justify="space-between" _hover={{bg:"#000000"}}><Image p={2} ml={{sm:200,lg:120}} src="/images/Graphic Design.png" height={{sm:"100" ,lg:"180" }}justify="flex-end"/><Text mt={{sm:10,lg:120}} p={2} color="white" fontWeight="900" justify="flex-end" alignItems="flex-end">Graphic Design</Text></GridItem>
+            <GridItem  p={2}  borderRadius={5} rowSpan={{sm:1,md:1,lg:2}} colSpan={2} bg='#755CDE' direction='row' justify="space-between" _hover={{bg:"#000000"}}><Image p={2} ml={{sm:200,lg:120}} src="/images/Graphic Design.png" height={{sm:"100" ,lg:"180" }}justify="flex-end"/><Text mt={{sm:10,lg:120}} p={2} color="white" fontWeight="900" justify="flex-end" alignItems="flex-end">Graphic Design</Text></GridItem>
             <GridItem  p={2} borderRadius={5} colSpan={1} bg='#F6A560' _hover={{bg:"#74746E"}}><Image p={2}  ml={61}  src="/images/ui.png" width="55" height="55" justify="flex-end"/><Text mt={20} color="white" fontWeight="900" fontSize="lg">UX/UI</Text> </GridItem>
             <GridItem p={2} borderRadius={5} colSpan={1} bg='#F39E9E' _hover={{bg:"#74746E"}}><Image p={2} ml={61} src="/images/apps.svg" width="55" height="55"  justify="flex-end"/><Text mt={20} color="white" fontWeight="900">Apps</Text> </GridItem>
             <GridItem p={2} borderRadius={5} colSpan={2} bg='#61C4B7' _hover={{bg:"#74746E"}}><Image p={2}  ml={140} src="/images/photo.png" width="55" height="55"justify="flex-end"/><Text mt={20} color="white" fontWeight="900">Photography</Text> </GridItem>
@@ -49,18 +49,18 @@ export default function Home() {
             <GridItem p={2} borderRadius={5} colSpan={2} bg='#552049' _hover={{bg:"#74746E"}}><Image p={2}  ml={160} src="/images/motion.png" width="55" height="55" justify="flex-end"/><Text mt={20} color="white" fontWeight="900">Motion Graphics</Text> </GridItem>
           </Grid>
 
-          <Flex mt={20} display="flex" justify="space-around" align={{sm:"center"}} direction={{sm:"column" ,md:"column",lg:"row" }}>
+          <Flex mt={{sm:20, md:50, lg:300}} display="flex" justify="space-around" align={{sm:"center"}} direction={{sm:"column" ,md:"column",lg:"row" }}>
             <MotionFlex whileHover={{ scale: 1.1}}  whileTap={{ scale: 0.9 }} >
-               <Image justify={{sm:"center"}} src="/images/alvin.png" h={{sm:"300",md:"400",lg:"440"}}  />
+               <Image  justify={{sm:"center"}} src="/images/alvin.png" h={{sm:"300",md:"400",lg:"440"}}  />
             </MotionFlex> 	
             <Flex w={{sm:"100%",lg:"30%"}} display="flex" align={{sm:"center"}}justify="center" direction="column">
-              <Heading mt="20" textAlign={{sm:"center"}}fontStyle="bold">I’m Alvin, and I’d love to work on your next project</Heading>
-              <Text mt={5} textAlign={{sm:"center"}} color="#7A746E">I love working with others to create beautiful design solutions. I’ve designed everything from brand illustrations to complete mobile apps. I’m also handy with a camera!</Text>
-              <Button mt={5} p={6} w="228px" h="56px" borderRadius="30px" color="white" bg="#EB7565" _hover={{ bg: '#F6A560' }} >Free Consultation </Button>
+              <Heading mt="10" textAlign={{sm:"center"}}fontStyle="bold">I’m Alvin, and I’d love to work on your next project</Heading>
+              <Text mt={7} textAlign={{sm:"center"}} color="#7A746E">I love working with others to create beautiful design solutions. I’ve designed everything from brand illustrations to complete mobile apps. I’m also handy with a camera!</Text>
+              <Button mt={7} p={6} w="228px" h="56px" borderRadius="30px" color="white" bg="#EB7565" _hover={{ bg: '#F6A560' }} >Free Consultation </Button>
             </Flex>
           </Flex>
 
-          <Heading  fontWeight="800"p={22}>My Work</Heading>
+          <Heading mt={10} fontWeight="800"p={22}>My Work</Heading>
              
 
 								<HStack id={`hscroll`}
@@ -71,7 +71,7 @@ export default function Home() {
 								scrollSnapType="x mandatory"
 								position="relative"
 								zIndex="docked"
-                p={22}
+               
 								>
 								{images.map((src) => (
 									<Image
