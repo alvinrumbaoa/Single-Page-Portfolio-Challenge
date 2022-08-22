c0291eac4b9b5a4ae4d43de74aad46841eae9dd0
Alvin Rumbaoa <alvinrumbaoa@gmail.com>
2022-08-21 19:13:19 -0700

    nav design

--- frontend/components/Nav.jsx
+++ frontend/components/Nav.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 import { Flex, Button, Link, Text, Heading, Image, Container, HStack, VStack, Stack, Grid } from '@chakra-ui/react'
 const Nav = () => {
 	return (
-			<Flex p={10} direction="row" justify="space-between">
+			<Flex p={20	} direction="row" justify="space-between">
 				<Link href="/"><Image src="/images/logo.svg" /></Link>
-				<Button p={10} w="228px" h="56px" borderRadius="30px" bgColor="#030303" _hover={{bg:'#755CDE'}}color="#FFF7F0">Free Consultation</Button>
+				<Button p={6} w="228px" h="56px" borderRadius="30px" bgColor="#030303" _hover={{bg:'#755CDE'}}color="#FFF7F0">Free Consultation</Button>
 			</Flex>
 	)
 }
